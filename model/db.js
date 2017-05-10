99b13a06dfa2a9707b1cd9ad68864ee7ef32928c
hw94 <haseebwajid94@gmail.com>
2017-05-10 15:30:02 +0500

    db

--- model/db.js
+++ model/db.js
@@ -1,3 +1,5 @@
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_db');
+//mongoose.connect('mongodb://localhost/my_db');
+mongoose.connect('mongodb://heroku_k7zvkxmv:dv0fa9j5ooinn27p05uk7di6ld@ds137291.mlab.com:37291/heroku_k7zvkxmv');
+
